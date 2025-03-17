d81764981ae8f813be952f554091d197aed99204
gela pipia <gelapipia10@gmail.com>
2025-03-17 17:18:00 +0400

    UPDATED and styled Task component

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,21 +1,29 @@
 const Task = ({ task }) => {
   return (
     <div className={`task task-${task.status.id}`}>
-        <div className="flex justify-between">
-            <div className="flex gap-[10px]">
-                <span className="flex items-center gap-[4px] border-[0.5px] border-[#08A508] text-[12px] p-[4px] rounded-[5px] min-w-[86px]"><img src={task.priority.icon} alt="priority iocn" />{task.priority.name}</span>
-                <span className="bg-[#FD9A6A] rounded-[15px] text-white min-w-[88px] py-[5px] px-[9px] text-center text-[12px]">მარკეტინგი</span>
-            </div>
-            <div>
-                {task.due_date.split("T")[0]}
-            </div>
+      <div className="flex justify-between">
+        <div className="flex gap-[10px]">
+          <span className={`task-priority task-priority-${task.priority.id}`}>
+            <img src={task.priority.icon} alt="priority icon" />
+            {task.priority.name}
+          </span>
+          <span className="departament-badge departament-badge-2">
+            მარკეტინგი
+          </span>
         </div>
-        
-      <h3>Name: {task.name}</h3>
-      <h3>Desc: {task.description}</h3>
-      <h3>Employee: {task.employee.name}</h3>
-      <h3>Priority: {task.priority.name}</h3>
-      <h3>Due date: {task.due_date}</h3>
+        <div className="text-nowrap">{task.due_date.split("T")[0]}</div>
+      </div>
+      <div>
+        <h3>{task.name}</h3>
+        <p className="mt-[12px]">{task.description}</p>
+      </div>
+      <div className="flex justify-between">
+        <img src={task.employee.avatar} width={32} height={32} className="rounded-full" alt="emploee avatar" />
+        <div className="flex items-center gap-[4px]">
+          <img src="./src/assets/comments-icon.svg" alt="comment icon" />
+          <span>{task.total_comments}</span>
+        </div>
+      </div>
     </div>
   );
 };
